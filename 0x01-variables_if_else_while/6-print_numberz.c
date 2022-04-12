ad5a54a3407a8e03a4ad4d1e24cd5b369ec2d32b
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-12 01:35:13 -0700

    First  attempt on Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - print numbers from 0 to 9
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
