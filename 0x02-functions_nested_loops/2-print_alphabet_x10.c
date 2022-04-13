fe75efaf8da72054c8d16fa0868f224f4d4f0651
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-13 12:05:05 -0700

    First attempt on task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/* 
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Doesn't return anything
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
