ae991fb456a4b6a0374d580f10d84b88d0f37944
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-13 14:37:28 -0700

    First attempt on  Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha: checks for alphabetic character
+ *
+ * Return: (1) Success, (0) Otherwise
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
