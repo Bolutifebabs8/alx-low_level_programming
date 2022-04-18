0c4c26f36e44e7564b93f8031104f637d0c603ab
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-18 15:09:00 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 prints natural numbers from n to 98
+ *
+ * Return: Doesn't return anything
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 10)
+			{
+				putchar(i + '0');
+			}
+			else
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+			}
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+		}
+		if (i != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
