ecd5935034c430d41f5f8ecf332b33e5415c4445
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-20 00:25:52 -0700

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long a = 1;
+	long b = 2;
+	long c;
+	int i;
+
+	printf("%ld, %ld, ", a, b);
+
+	for (i = 0; i < 96; i++)
+	{
+		c = a + b;
+		if (i = 95)
+		{
+			printf("%ld", c);
+		}
+		else
+		{
+			printf("%ld, ", c);
+		}
+		a = b;
+		b = c;
+	}
+	putchar('\n');
+	return (0);
+}
