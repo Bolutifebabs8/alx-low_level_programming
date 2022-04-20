ab11f34d8eaea83f547301090fe8c459b912fcd8
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-20 00:10:58 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of the even-valued terms,
+ * whose values do not exceed 4000000
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c;
+	long sum = 2;
+
+	do
+	{
+		c = a + b;
+		if ((c % 2) == 0)
+		{
+			sum += c;
+		}
+		a = b;
+		b = c;
+	}
+	while (c < 4000000);
+
+	printf("%ld\n", sum);
+}
