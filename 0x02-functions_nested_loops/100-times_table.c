d541bacdc5293041fde16408a2d0af90aa38bd1c
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-19 22:42:14 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,16 +14,16 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)	
+		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-			
+
 			for (b = 1; b <= n; b++)
 			{
 				op = a * b;
 				_putchar(44);
 				_putchar(32);
-			
+
 				if (op <= 9)
 				{
 					_putchar(32);
