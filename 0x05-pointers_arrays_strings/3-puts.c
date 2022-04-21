219f6f528f806dfb946c24c1e34cfab427b6a20d
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-20 17:59:28 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be printed.
+ *
+ * Return: Doesn't return anything
+ */
+
+void _puts(char *str)
+{
+	while (*s != '\0')
+	{
+		putchar(*s);
+		s++;
+	}
+	putchar('\n');
+}
