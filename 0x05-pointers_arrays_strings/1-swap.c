3b170e576155300d9f5bcc70e588bc0bf996b978
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-20 17:19:43 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int: swaps the values of two integers.
+ * swap_int - swaps the values of two integers.
  *
  * @a: value to be evaluate.
  * @b: value to be evaluate.
