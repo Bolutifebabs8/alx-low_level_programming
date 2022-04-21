7a7607b4e1a7f377aeeaaaa51aa3a6faf4beaf5f
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-21 15:33:14 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse.
+ *
+ * @s: String to be printed
+ *
+ * Return: Doesn't return anything
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(*s); i >= 0; i--)
+	{
+		putchar(*s[i]);
+	}
+}
