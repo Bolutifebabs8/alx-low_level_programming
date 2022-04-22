fc1788e83b567043598649d21168b13de4a4ec01
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-21 17:34:38 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string, 
+ * starting with the first character.
+ *
+ * @str: String to be printed.
+ *
+ * Return: Doesn't return anything.
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int k = strlen(str);
+
+	for (i = 0; i < k; i++)
+	{
+		if ((s[i] % 2) == 0)
+		{
+			putchar(s[i]);
+		}
+	}
+	putchar('\n');
+}
