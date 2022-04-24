bd1929d04693407a345bba192e8429634265a521
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-24 13:45:02 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,15 @@
 
 char *string_toupper(char *c)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (c[i] != '\0')
 	{
 		if (c[i] >= 'a' && c[i] <= 'z')
 		{
-			c[i] = c[i] + 32;
+			c[i] -= 32;
 		}
 		i++;
 	}
