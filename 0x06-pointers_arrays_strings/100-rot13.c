ccb28ed01266728acc2bbdf21b4a366be9943abf
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-24 16:01:43 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main'h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @c: string to be encoded
+ *
+ * Return: returns a character.
+ */
+
+char *rot13(char *c)
+{
+	int i, j;
+
+	char x[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char y[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == x[j])
+			{
+				c[i] = y[j];
+			}
+		}
+	}
+	return (c);
+}
