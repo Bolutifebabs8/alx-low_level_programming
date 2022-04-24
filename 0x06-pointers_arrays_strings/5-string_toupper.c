36be68b2a877c94588c58b2d1bf9e3becf4b83bf
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-24 13:29:38 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper -  changes all lowercase letters of a string to uppercase.
+ *
+ * @c: array to be examined
+ *
+ * Return: returns a character
+ */
+
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			c[i] += 32;
+		}
+		i++;
+	}
+}
