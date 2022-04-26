92ed7881224acbd7c765f340ceaa4f956a0b755f
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-04-26 16:25:57 -0700

    Task 5 again

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @haystack: string to be searched
+ * @needle: String to be found.
+ *
+ * Return: Returns a character.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	i = strstr(haystack, needle);
+
+	return (i);
+}
