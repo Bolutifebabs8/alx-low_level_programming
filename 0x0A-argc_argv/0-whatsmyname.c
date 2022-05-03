51755afc7baf9aff068bcc165b069b8b8bbecf31
Bolutifebabs8 <Bolutifebabs8@gmail.com>
2022-05-02 18:13:45 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ *
+ * @argc: number of integers
+ * @argv: array of strings.
+ *
+ * Return: Returns either 0 or 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0])
+}
